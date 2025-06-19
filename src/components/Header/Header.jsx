8f4957be9e309aae16efb5116fa1e9dea9c99990
name <salmanmgr811201@gmail.com>
2025-06-20 00:01:40 +0530

    adding file

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Header.css';
+
+const Header = () => {
+  return (
+    <div class="header">
+      <div className="header-content">
+        <h2>Order your favourite food here</h2>
+        <p></p>
+        <button>Vew</button>
+      </div>
+    </div>
+  )
+}
+
+export default Header
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Home.css';
+
+const Home = () => {
+  return (
+    <div>
+      <h1> Hello</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus ea, nisi impedit autem sit doloremque inventore quod dolor reiciendis dolorum?</p>
+    </div>
+  )
+}
+
+export default Home
