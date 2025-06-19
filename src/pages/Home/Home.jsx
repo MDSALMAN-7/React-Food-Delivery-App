b53cd1e2cd3e4b263bb7d06eff5a709c985e04cc
name <salmanmgr811201@gmail.com>
2025-06-20 00:08:34 +0530

    checking

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -4,8 +4,7 @@ import './Home.css';
 const Home = () => {
   return (
     <div>
-      <h1> Hello</h1>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus ea, nisi impedit autem sit doloremque inventore quod dolor reiciendis dolorum?</p>
+      
     </div>
   )
 }
